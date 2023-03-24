cfc8186e7c07e1c5259c5e789056582e5809c8d4
Nephim <39805658+Nephim@users.noreply.github.com>
2023-03-24 01:39:55 +0100

    Initial commit

---
+++ Code_03/Code_03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Solution s = new Solution();
+
+s.LengthOfLongestSubstring("dvdf");
+
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int ans = 0;
+        var map = new Dictionary<char, int>();
+
+        int j = 0;
+        for (int i = 0; i < s.Length; i++) 
+        {
+            char c = s[i];
+            if(map.ContainsKey(c))
+            {
+                ans = ans > j ? ans : j;
+
+                map.TryGetValue(c, out i);
+                i++;
+                c = s[i];
+                map = new Dictionary<char, int>();
+                j = 0;
+            }
+
+            map.Add(c, i);
+            j++;
+        }
+
+        ans = ans > j ? ans : j;
+
+        return ans;
+    }
+}
